66422ffe65a2e51baa315b535ac97b89baa01c67
Hamidreza Sadeghi <sadeghi.hamidreza1400@gmail.com>
2022-12-14 16:10:47 +0330

    fix bugs

--- speaker_diarization.py
+++ speaker_diarization.py
@@ -12,12 +12,5 @@ stream_ssd_calss = Stream_SSD(user_voice_path)
 stream_ssd_calss.load_clf_model(os.path.join(BASE_DIR.parent,'saved_model'))
 
 def speech_diarization(audio_byte):
-    audio_bufers = np.frombuffer(audio_byte, dtype=np.int16)
-    speaker_lable_list = []
-    for index in range(0, int(len(audio_bufers) / (sample_rate * stride))):
-        audio_bufer = audio_bufers[int(index * stride * sample_rate) : int((index+1) * (stride * sample_rate))]
-        audio_byte = audio_bufer.tobytes()
-        speech_lable = stream_ssd_calss.recognize(audio_byte)
-        speaker_lable_list.append(speech_lable)
-    speaker = max(set(speaker_lable_list), key=speaker_lable_list.count) if speaker_lable_list != [] else 'non-speech'
+    speaker = stream_ssd_calss.recognize(audio_byte)
     return speaker
